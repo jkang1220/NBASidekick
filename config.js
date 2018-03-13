5e014669c58973da740924157e865693dfb9ee63
Jimmy Kang <jimmykang@Jimmys-MacBook-Pro.local>
2018-03-12 23:43:30 -0400

    Updated config vars for deployment

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
 module.exports = {
   auth: {
-    username: '......',
-    password: '......'
+    username: process.env.api_username,
+    password: process.env.api_password
   },
   database: {
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'nbasidekick'
+    host     : process.env.db_host,
+    user     : process.env.db_user,
+    port: 3306,
+    password : process.env.db_password,
+    database: 'nbasidekick'
   }
 }
